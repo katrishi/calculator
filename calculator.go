4c5be39fb0166b95b3eedaec8858a74c27c157a7
katrishi <katrishi@tut.by>
2023-09-11 22:41:31 +0300

    calculate arabic numerals

--- calculator.go
+++ calculator.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var a, operator, b, c string
 
-	fmt.Println("введите значение в формате: 1 + 5 или V * VII")
+	fmt.Println("введите значение в формате: 1 + 5")
 	fmt.Scanln(&a, &operator, &b, &c)
 
 	if c != "" {
@@ -14,5 +17,32 @@ func main() {
 
 	if a == "" || operator == "" || b == "" {
 		panic("не является математической операцией")
+	}
+
+	_a, okA := strconv.Atoi(a)
+	_b, okB := strconv.Atoi(b)
+
+	if okA == nil && okB == nil {
+		if (_a > 0 && _a <= 10) && (_b > 0 && _b <= 10) {
+			res := strconv.Itoa(operate(_a, _b, operator))
+			fmt.Println(res)
+		} else {
+			fmt.Println("только целые положительные числа от 1 до 10 включительно")
+		}
+	}
+}
+
+func operate(a int, b int, operator string) int {
+	switch operator {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "/":
+		return a / b
+	case "*":
+		return a * b
+	default:
+		panic("только +, -, /, *")
 	}
 }
